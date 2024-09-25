5415fd9ca0a73d7edb2b3d4db2f085e91d694159
ffais <ffais@fbk.eu>
2024-09-25 14:17:45 +0200

    fix and remove license
    Signed-off-by: ffais <ffais@fbk.eu>

--- wordpress/confs/wordpress/update-script.sh
+++ wordpress/confs/wordpress/update-script.sh
@@ -2,26 +2,26 @@
 echo "Activating maintenance mode..."
 wp maintenance-mode activate
 
-{{- if .Values.jobs.update.core -}}
+{{ if .Values.jobs.update.core -}}
 echo "Updating Wordpress core..."
 wp core update
 wp core update-db
-{{- end -}}
+{{- end }}
 
-{{- if .Values.jobs.update.plugin -}}
+{{ if .Values.jobs.update.plugins -}}
 echo "Updating all plugins..."
 wp plugin update --all
-{{- end -}}
+{{- end }}
 
-{{- if .Values.jobs.update.themes -}}
+{{ if .Values.jobs.update.themes -}}
 echo "Updating all themes..."
 wp theme update --all
-{{- end -}}
+{{- end }}
 
-{{- if .Values.jobs.update.languages -}}
+{{ if .Values.jobs.update.languages -}}
 echo "Updating Wordpress core and themes languages..."
 wp language core update
 wp language theme update --all
-{{- end -}}
+{{- end }}
 
 wp maintenance-mode deactivate
